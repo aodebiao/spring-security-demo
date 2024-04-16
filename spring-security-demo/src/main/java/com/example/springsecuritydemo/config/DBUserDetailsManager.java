cde95679f1cac5a5eeef81e04603224dc0366925
Garua <582829957@qq.com>
2024-04-16 22:36:56 +0800

    db config

--- spring-security-demo/src/main/java/com/example/springsecuritydemo/config/DBUserDetailsManager.java
+++ spring-security-demo/src/main/java/com/example/springsecuritydemo/config/DBUserDetailsManager.java
@@ -10,13 +10,14 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
 
 
-@Configuration
+@Component
 @RequiredArgsConstructor
 public class DBUserDetailsManager implements UserDetailsService {
 
